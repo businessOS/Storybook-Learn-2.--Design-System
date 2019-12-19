280a795cfe62b7bfa7ae43bcb7ff7e609c653972
Helder Bruno Araujo Silva <hbas.developer@gmail.com>
2019-12-19 12:11:42 -0500

    documentation

--- learnstorybook-design-system/.storybook/config.js
+++ learnstorybook-design-system/.storybook/config.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { configure, addDecorator } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
+import "storybook-chromatic";
+
 import { GlobalStyle } from "../src/shared/global";
 
 addDecorator(withA11y);
@@ -11,5 +13,11 @@ addDecorator(story => (
   </>
 ));
 
-// automatically import all files ending in *.stories.js
-configure(require.context("../src", true, /\.stories\.js$/), module);
+// automatically import all files ending in *.stories.js|mdx
+configure(
+  [
+    require.context("../src", false, /Intro\.stories\.mdx/),
+    require.context("../src", true, /\.stories\.(js|mdx)$/)
+  ],
+  module
+);
